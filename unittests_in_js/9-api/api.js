1090ed16e8e5043d3445f42e44332e15abdb26eb
Naveen@brightcomgroup <naveenkumarr@brightcomgroup>
2023-04-07 11:08:09 +0000

    l

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -7,3 +7,4 @@ app.get('/', (req, res) => res.end('Welcome to the payment system'))
 app.get('/cart/:id([0-9]+)', (req, res) => {
   res.send(`Payment methods for cart ${req.params.id}`)
 })
+app.listen(port, () => console.log(`API available on localhost port ${port}`));
